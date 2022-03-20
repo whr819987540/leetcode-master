273994a0e69500ed2dfc07cf4b100a6b9e08ef72
whr <819987540@qq.com>
2022-03-20 23:45:34 +0800

    dp-完全-排列-引入参数m

--- c_code/动态规划/背包/完全背包/3-70-爬楼梯方法数.cpp
+++ c_code/动态规划/背包/完全背包/3-70-爬楼梯方法数.cpp
@@ -27,7 +27,37 @@ public:
         return dp[n];
     }
 };
-
+// 引申
+// 楼梯数n，每次可以走1/2/3/m步
+// 还是问爬到顶的方法数
+// n为背包容量，m为物品
+// 物品显然可重复，所以是完全背包。背包容量递增
+// 因为12和21不同，排列，所以先遍历背包容量，再遍历物品
+// 否则就等价于提前规定好物品的顺序了
+// 1、数组含义：dp[i]表示走到第i层的方法数
+// 2、递推公式：dp[i] = dp[i-1] + dp[i-2] + dp[i-m]
+// 3、初始化：dp[0] = 1
+// 4、遍历顺序：先背包容量（递增），再物品
+class Solution
+{
+public:
+    int climbStairs(int n, int m)
+    {
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        for (int j = 0; j <= n; j++)
+        {
+            for (int i = 1; i <= m; i++)
+            {
+                if (j >= i)
+                {
+                    dp[j] = dp[j] + dp[j - i];
+                }
+            }
+        }
+        return dp[n];
+    }
+};
 
 int main()
 {
