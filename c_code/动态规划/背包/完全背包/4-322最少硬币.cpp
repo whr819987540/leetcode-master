3a1a4f8eee6232fb9b0684f167b74bfec0ac9441
whr <819987540@qq.com>
2022-03-22 08:57:52 +0800

    dp-完全-dp[0]=0，因为是最小，所以其他值初始化为INT_MAX

--- c_code/动态规划/背包/完全背包/4-322最少硬币.cpp
+++ c_code/动态规划/背包/完全背包/4-322最少硬币.cpp
@@ -68,6 +68,54 @@ public:
     }
 };
 
+// 1、数组含义：dp[i]表示构成i的最小硬币数
+// 2、递推公式：dp[i] = min(dp[i], dp[i-coins] + 1)
+// 3、初始化：dp[0]表示构成和为0的最小硬币数，dp[0] = 0
+// 因为是求最小值，所以其他值不能初始化为0，应该初始化为INT_MAX
+// 这样就可以在比较过程中自动识别不可行的方法，而不用逐个判断
+// 4、遍历顺序：完全背包，所以背包容量递增
+// 不涉及排列组合，所以背包和物品的遍历顺序不影响结果
+class Solution2
+{
+public:
+    int coinChange(vector<int> &coins, int amount)
+    {
+        // INT_MAX作为标志位，表示构成和为j的方法不存在
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        display(dp);
+        for (int i = 0; i < coins.size(); i++)
+        {
+            for (int j = coins[i]; j <= amount; j++)
+            {
+                // dp[j] = min(dp[j], dp[j - coins[i]] + 1);
+                // dp[j - coins[i]] + 1可能溢出，变成负值，那自然是最小值
+                // 如n=3 [2]
+                // 0 INT_MAX INT_MAX INT_MAX
+                // 0 INT_MAX min(INT_MAX,0+1) min(INT_MAX,INT_MAX+1)
+                // 如果溢出为负值，表示仍然不可行
+
+                //没有溢出，j-coins[i]!=INT_MAX，即j-coins[i]可行
+                if (dp[j - coins[i]] != INT_MAX) // 保证不溢出
+                {
+                    dp[j] = min(dp[j], dp[j - coins[i]] + 1);
+                }
+                // 如果溢出，j-coins[i]也不可行，所以dp[j]保留原值
+            }
+            display(dp);
+        }
+        return dp[amount] != INT_MAX ? dp[amount] : -1;
+    }
+    void display(const vector<int> &data)
+    {
+        for (auto i : data)
+        {
+            cout << i << " ";
+        }
+        cout << "\n---------------------\n";
+    }
+};
+
 int main()
 {
     {
@@ -88,6 +136,30 @@ int main()
 
     {
         Solution s;
+        vector<int> data{1};
+        int n = 0;
+        int res = s.coinChange(data, n);
+        cout << res << endl;
+    }
+
+    {
+        Solution2 s;
+        vector<int> data{1, 2, 5};
+        int n = 11;
+        int res = s.coinChange(data, n);
+        cout << res << endl;
+    }
+
+    {
+        Solution2 s;
+        vector<int> data{2};
+        int n = 3;
+        int res = s.coinChange(data, n);
+        cout << res << endl;
+    }
+
+    {
+        Solution2 s;
         vector<int> data{1};
         int n = 0;
         int res = s.coinChange(data, n);
