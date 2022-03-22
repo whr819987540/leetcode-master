53151832b1bfe736307617a5f27d45ca9990799d
whr <819987540@qq.com>
2022-03-22 19:04:28 +0800

    两次dp，不同时考虑第一家和最后一家

--- c_code/动态规划/打家劫舍/2-213-打家劫舍.cpp
+++ c_code/动态规划/打家劫舍/2-213-打家劫舍.cpp
@@ -58,11 +58,54 @@ public:
     }
 };
 
+// 不能同时偷第0家和最后一家
+// 最简单的方法就是，让这两家不同时出现现在nums序列中
+// 1、数组含义：dp[i]表示偷到第i家时获得的最大价值
+// 2、递推公式：dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+// 3、初始化：dp[0]=nums[0],dp[1] = max(nums[0], nums[1])
+// 4、遍历顺序：递增
+class Solution2
+{
+public:
+    int rob(vector<int> &nums)
+    {
+        if (nums.size() == 1)
+        {
+            return nums[0];
+        }
+        vector<int> strip_last;
+        strip_last.assign(nums.begin(), nums.begin() + nums.size() - 1);
+        vector<int> strip_zero;
+        strip_zero.assign(nums.begin() + 1, nums.end());
+        return max(func(strip_zero), func(strip_last));
+    }
+    // 从nums序列中找到最大价值，此时不需要考虑环
+    int func(vector<int> &nums)
+    {
+        if (nums.size() == 1)
+        {
+            return nums[0];
+        }
+        vector<int> dp(nums.size());
+        dp[0] = nums[0], dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size(); i++)
+        {
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+        return dp.back();
+    }
+};
+
 int main()
 {
     {
-        Solution s;
+        Solution2 s;
         vector<int> data = {2, 3, 2};
+        cout << s.rob(data) << endl;
+    }
+    {
+        Solution2 s;
+        vector<int> data = {2, 2, 4, 3, 2, 5};
         cout << s.rob(data) << endl;
     }
 }
