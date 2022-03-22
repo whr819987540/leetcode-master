1ca1ff29745a549287321a1d7c0a9d7341febbb0
whr <819987540@qq.com>
2022-03-22 09:37:33 +0800

    dp，先遍历容量，后遍历背包，初始化为INT_MAX

--- c_code/动态规划/背包/完全背包/4-322最少硬币.cpp
+++ c_code/动态规划/背包/完全背包/4-322最少硬币.cpp
@@ -116,11 +116,41 @@ public:
     }
 };
 
+// 这个题不在意顺序，所以也可以先遍历背包容量，再遍历物品
+
+// 1、数组含义：dp[i]表示构成i的最小硬币数
+// 2、递推公式：dp[i] = min(dp[i], dp[i-coins] + 1)
+// 3、初始化：dp[0]表示构成和为0的最小硬币数，dp[0] = 0
+// 因为是求最小值，所以其他值不能初始化为0，应该初始化为INT_MAX
+// 这样就可以在比较过程中自动识别不可行的方法，而不用逐个判断
+// 4、遍历顺序：完全背包，所以背包容量递增
+// 先背包容量，后遍历物品
+class Solution3
+{
+public:
+    int coinChange(vector<int> &coins, int amount)
+    {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int j = 1; j <= amount; j++)
+        {
+            for (int i = 0; i < coins.size(); i++)
+            {
+                if (j >= coins[i] && dp[j - coins[i]] != INT_MAX)
+                {
+                    dp[j] = min(dp[j], dp[j - coins[i]] + 1);
+                }
+            }
+        }
+        return dp[amount] != INT_MAX ? dp[amount] : -1;
+    }
+};
+
 // 用回溯来解决
 // 需要遍历整棵树，找到可行解path，然后更新最短路径(初始化为INT_MAX)
 // 横向遍历：硬币没有重复值，不需要剪枝
 // 纵向遍历：硬币可以重复（可以找自己），不要求顺序（不能回头）
-class Solution3
+class Solution4
 {
 private:
     vector<int> path;
@@ -210,7 +240,6 @@ int main()
         int res = s.coinChange(data, n);
         cout << res << endl;
     }
-
     {
         Solution3 s;
         vector<int> data{1, 2, 5};
@@ -221,6 +250,30 @@ int main()
 
     {
         Solution3 s;
+        vector<int> data{2};
+        int n = 3;
+        int res = s.coinChange(data, n);
+        cout << res << endl;
+    }
+
+    {
+        Solution3 s;
+        vector<int> data{1};
+        int n = 0;
+        int res = s.coinChange(data, n);
+        cout << res << endl;
+    }
+    
+    {
+        Solution4 s;
+        vector<int> data{1, 2, 5};
+        int n = 11;
+        int res = s.coinChange(data, n);
+        cout << res << endl;
+    }
+
+    {
+        Solution4 s;
         vector<int> data{2};
         int n = 3;
         int res = s.coinChange(data, n);
