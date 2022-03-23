ecc7c4993a7e7394277dac3ca730c4f71e9ccafb
whr <819987540@qq.com>
2022-03-23 21:34:39 +0800

    一维数组，内层遍历顺序可能不对

--- c_code/动态规划/子序列/4-1143-最长公共子序列.cpp
+++ c_code/动态规划/子序列/4-1143-最长公共子序列.cpp
@@ -50,11 +50,84 @@ private:
     }
 };
 
+// 上面的二维数组+遍历二维数组的子数组超时
+// 压缩为一维数组，然后对一维数组的子数组遍历，找最大值
+// 1、数组含义：dp[j]表示第二个字符串以j结尾时最长公共子序列的长度
+// 2、递推公式：if text1[i]==text2[j],dp[j]=max(dp[n])+1,n<j
+// 否则维持原值（必须维持原值，因为dp数组需要记录历史结果）
+// 为什么能这样？因为虽然dp数组定义为以j结尾，但是压缩后，可以不以j结尾
+// 因为dp数组还用来记录历史结果
+// 3、初始化：0
+// 4、遍历顺序：递增，递增
+class Solution2
+{
+public:
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int max_length = 0;
+        vector<int> dp(text2.size(), 0);
+
+        for (int i = 0; i < text1.size(); i++)
+        {
+            for (int j = 0; j < text2.size(); j++)
+            {
+                if (text1[i] == text2[j])
+                {
+                    dp[j] = get_max(dp, j) + 1;
+                    max_length = max(max_length, dp[j]);
+                }
+            }
+        }
+        return max_length;
+    }
+
+private:
+    int get_max(const vector<int> &dp, int j)
+    {
+        int res = 0;
+        for (int i = 0; i < j; i++)
+        {
+            res = max(res, dp[i]);
+        }
+        return res;
+    }
+};
+
 int main()
 {
     {
         Solution s;
         string text1 = "abcde", text2 = "aecd";
-        cout << s.longestCommonSubsequence(text1, text2);
+        cout << s.longestCommonSubsequence(text1, text2) << endl;
+    }
+
+    {
+        Solution s;
+        string text1 = "abc", text2 = "abc";
+        cout << s.longestCommonSubsequence(text1, text2) << endl;
+    }
+
+    {
+        Solution s;
+        string text1 = "abcba", text2 = "abcbcba";
+        cout << s.longestCommonSubsequence(text1, text2) << endl;
+    }
+
+    {
+        Solution2 s;
+        string text1 = "abcde", text2 = "aecd";
+        cout << s.longestCommonSubsequence(text1, text2) << endl;
+    }
+
+    {
+        Solution2 s;
+        string text1 = "abc", text2 = "abc";
+        cout << s.longestCommonSubsequence(text1, text2) << endl;
+    }
+
+    {
+        Solution2 s;
+        string text1 = "abcba", text2 = "abcbcba";
+        cout << s.longestCommonSubsequence(text1, text2) << endl;
     }
 }
