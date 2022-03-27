7c468f9f3e7aa88b5e07eb557fabaf91a2733e35
whr <819987540@qq.com>
2022-03-27 22:01:35 +0800

    dp-一维数组

--- c_code/动态规划/子序列/6-53-最大子数组和.cpp
+++ c_code/动态规划/子序列/6-53-最大子数组和.cpp
@@ -32,6 +32,40 @@ public:
     }
 };
 
+// 1、数组含义：dp[i]表示一定放第i个数时，最大子数组和
+// 这样能保证连续
+// 2、递推公式：dp[i]=max(dp[i-1]+nums[i],nums[i])
+// 因为必须放第i个数，所以，要么接上第i-1个数，要么不接上
+// 这个和674有点像，都是对一个数组进行操作
+// 3、初始化：dp[0]=nums[0]
+// 4、遍历顺序：递增
+class Solution2
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        int res = dp[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            // if (dp[i - 1] > 0)
+            // {
+            //     dp[i] = dp[i - 1] + nums[i];
+            // }
+            // else
+            // {
+            //     dp[i] = nums[i];
+            // }
+            dp[i] = max(nums[i], dp[i - 1] + nums[i]);
+            res = max(res, dp[i]);
+        }
+        // return dp[nums.size() - 1];
+        // 最大子数组和不一定以最后一个元素结尾
+        return res;
+    }
+};
+
 int main()
 {
     {
@@ -48,6 +82,24 @@ int main()
 
     {
         Solution s;
+        vector<int> data = {-2, -3, -1};
+        cout << s.maxSubArray(data) << endl;
+    }
+
+    {
+        Solution2 s;
+        vector<int> data = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        cout << s.maxSubArray(data) << endl;
+    }
+
+    {
+        Solution2 s;
+        vector<int> data = {-2, 1, -3};
+        cout << s.maxSubArray(data) << endl;
+    }
+
+    {
+        Solution2 s;
         vector<int> data = {-2, -3, -1};
         cout << s.maxSubArray(data) << endl;
     }
