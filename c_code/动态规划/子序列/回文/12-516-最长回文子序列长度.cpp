f060e6f7ac87887f4dc216182db2a2ed4c851545
whr <819987540@qq.com>
2022-03-28 19:34:43 +0800

    dp-二维数组，表示最长回文子序列的长度

---
+++ c_code/动态规划/子序列/回文/12-516-最长回文子序列长度.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+// 给定字符串s，返回最长回文子序列的长度
+// 回文：正序和逆序相同
+// 子序列：不要求连续
+// Input: s = "bbbab"
+// Output: 4
+// Explanation: One possible longest palindromic subsequence is "bbbb".
+
+// 1、数组含义：dp[i][j]表示s以i开头、j结尾时最长回文子序列的长度
+// 2、递推公式：
+// j>=i
+// if s[i]==s[j]
+//      if i==j,dp[i][j]=1
+//      else if i==j-1,dp[i][j]=2
+//      else dp[i][j]=dp[i+1][j-1]+2
+// else dp[i][j]=max(dp[i+1][j],dp[i][j-1])
+// 3、初始化：0
+// 4、遍历顺序：递减，递增
+// 最后返回dp[0][s.size()-1]
+
+// 647不能直接将dp定义为结果，而本题可以，因为本题的递推公式是max
+// 不需要考虑是否重复
+// 而647是个数，需要考虑重复
+class Solution
+{
+public:
+    int longestPalindromeSubseq(string s)
+    {
+        vector<vector<int>> dp(s.size(), vector<int>(s.size(), 0));
+
+        for (int i = s.size() - 1; i >= 0; i--)
+        {
+            for (int j = i; j < s.size(); j++)
+            {
+                if (s[i] == s[j])
+                {
+                    if (i == j)
+                    {
+                        dp[i][j] = 1;
+                    }
+                    else if (j == i + 1)
+                    {
+                        dp[i][j] = 2;
+                    }
+                    else
+                    {
+                        dp[i][j] = dp[i + 1][j - 1] + 2;
+                    }
+                }
+                else
+                {
+                    // i、j不相等，只能删除一个
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][s.size() - 1];
+    }
+};
+
+int main()
+{
+    {
+        Solution s;
+        string data = "bbbab";
+        cout << s.longestPalindromeSubseq(data) << endl;
+    }
+
+    {
+        Solution s;
+        string data = "cbbd";
+        cout << s.longestPalindromeSubseq(data) << endl;
+    }
+}
